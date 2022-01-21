becb46601375f3abdeae2715ad4943616c9f5ee3
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-01-21 14:29:29 -0500

    HTML y el formulario para crear propiedades
    - Se los crea en el archivo crear.php

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -10,6 +10,48 @@
 
         <a href="/admin/" class="boton boton-verde">Volver</a>
 
+        <form action="" class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Título</label>
+                <input type="text" id="titulo" placeholder="Titulo de la Propiedad">
+
+                <label for="precio">Precio</label>
+                <input type="number" id="precio" placeholder="Precio de la Propiedad">
+
+                <label for="imagen">Imágen</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción</label>
+                <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Informacion de la Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños</label>
+                <input type="number" id="wc" placeholder="Ej: 2" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento: </label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 1" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="vendedores" id="vendedores">
+                    <option value="1">Juan</option>
+                    <option value="2">Karen</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
+
     </main>
 
 
