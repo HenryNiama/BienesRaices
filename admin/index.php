02ef6f4f18b8a25f66c411adefe5a46fc05298d9
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-01-26 23:36:45 -0500

    Creando una tabla para listar las propiedades de la BDD
    -Se edita el index.php, app.css, _botones.scss, _admin.scss

--- admin/index.php
+++ admin/index.php
@@ -16,11 +16,37 @@
 
 
     <main class="contenedor seccion">
+
         <h1>Administrador de Bienes Raices</h1>
             <?php if (intval($resultado) === 1): ?>
                 <p class="alerta exito">Anuncio creado correctamente.</p>
             <?php endif; ?>    
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+    
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa</td>
+                    <td><img src="" class="imagen-tabla" alt=""></td>
+                    <td>$3241241</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>            
+        </table>
+    
     </main>
 
 
