36f32035f94d6e11fbffaf7e5e0f6549daa25254
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-01-30 13:57:04 -0500

    Hashear Passwords
    Cuando hasheamos en php, se usa siempre el tipo de valor de char, con longitud 60, es decir, char(60) por que la funcion de hash, crea el password encryptado con 60 caracteres, entonces, char es para una longitud fija, y varchar para una longitud variable.

--- usuario.php
+++ usuario.php
@@ -4,13 +4,21 @@
 require 'includes/config/database.php';
 $db = conectarBD();
 
+
+
 //Crear un emai y password
 $email = "correo@correo.com";
 $password = "123456";
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+    //var_dump($passwordHash);
 
 //Query para crear el usuario
-$query = "INSERT INTO usuarios (email, password) VALUES ('${email}', '${password}');";
-    //echo $query;
+$query = "INSERT INTO usuarios (email, password) VALUES ('${email}', '${passwordHash}');";
+
+    // echo $query;
+
+    // exit;
 
 //Agregarlo a la base de datos
 mysqli_query($db, $query);
