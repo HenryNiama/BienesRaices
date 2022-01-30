cb5e1053abcccfa9e63155c10a137d0b79aad894
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-01-30 14:35:15 -0500

    validaciones al formulario

--- login.php
+++ login.php
@@ -1,4 +1,44 @@
 <?php 
+
+    require 'includes/config/database.php';
+
+    $db = conectarBD();
+
+
+    //Autenticar al usuario
+    $errores = [];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        // echo "<pre>";
+        // var_dump($_POST);
+        // echo "</pre>";
+
+        $email = mysqli_real_escape_string( $db, filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL));
+        $password = mysqli_real_escape_string( $db, $_POST['password']);
+    
+        if (!$email) {
+            $errores[] = "El email es obligatorio o no es valido.";
+        }
+
+        if (!$password) {
+            $errores[] = "El password es obligatorio.";
+        }
+
+        // echo "<pre>";
+        //  var_dump($errores);
+        // echo "</pre>";
+    
+        if (empty($errores)) {
+            
+        }
+    }
+
+
+
+
+
+
+    //Incluye el header
     require 'includes/funciones.php';
 
     incluirTemplate('header');
@@ -8,15 +48,21 @@
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar Sesión</h1>
 
-        <form class="formulario" action="">
+        <?php foreach($errores as $error):?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?>
+
+        <form class="formulario" method="POST" novalidate>
             <fieldset>
                 <legend>Email y Password</legend>
 
                     <label for="email">E-mail</label>
-                    <input type="email" placeholder="Tu email" id="email">
+                    <input type="email" name="email" placeholder="Tu email" id="email" required>
 
                     <label for="password">Password</label>
-                    <input type="password" placeholder="Tu password" id="password">
+                    <input type="password" name="password" placeholder="Tu password" id="password" required>
             </fieldset>
 
             <input type="submit" value="Sign In" class="boton boton-verde">
