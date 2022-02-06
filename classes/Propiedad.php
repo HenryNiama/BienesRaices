4c711f05d866db74bb3b73f59b6338954bf52c78
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-02-06 13:18:50 -0500

    Sanitizar entrada de datos

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -6,6 +6,8 @@ class Propiedad{
 
     //Base de Datos
     protected static $db;
+    protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
+
 
     public $id;
     public $titulo;
@@ -17,6 +19,13 @@ class Propiedad{
     public $estacionamiento;
     public $creado;
     public $vendedorId;
+
+
+    //Definir la conexion a la BD
+    public static function setDB($database){
+        self::$db = $database;
+    }
+
 
     public function __construct($args = [])
     {
@@ -35,21 +44,53 @@ class Propiedad{
 
     public function guardar()
     {
+
+        //Sanitizar los datos
+        $atributos = $this->sanitizarAtributos();
+        // debugear($atributos);
+
         //Insertar en la Base de Datos
         $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId)
                   VALUES ('$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', 
                 '$this->vendedorId')";
         
         //debugear($query);
-        //Como ya esta isntanciada y conectada nuestra base de datos con el metodo estiativo y $db, entonces:
+        //Como ya esta instanciada y conectada nuestra base de datos con el metodo estiativo y $db, entonces:
         $resultado = self::$db->query($query);
 
         debugear($resultado);
     }
 
-    //Definir la conexion a la BD
-    public static function setDB($database){
-        self::$db = $database;
+    //Este metodo se encarga de iterar $columnasDB[], identifica y une los atributos de la BD
+    public function iterarAtributos()
+    {
+        $atributos = [];
+
+        foreach (self::$columnasDB as $columna) {
+
+            if($columna === 'id') continue;
+
+            $atributos[$columna] = $this->$columna;
+        }
+
+        return $atributos;
+    }
+
+
+    //Sanitiza cada uno de los atributos que vienen del formulario con ayuda del metodo iterarAtributos
+    public function sanitizarAtributos()
+    {
+        $atributos = $this->iterarAtributos();
+        //debugear($atributos);
+
+        $sanitizado = [];
+
+        foreach ($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+
+        // debugear($sanitizado);
+        return $sanitizado;
     }
 
 
