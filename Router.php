d063a7504fccf285e477ff667d19e35734845896
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-02-28 12:14:15 -0500

    Asociacion URL's a metodos del Controlador

--- Router.php
+++ Router.php
@@ -24,10 +24,16 @@ class Router{
         // debugear($metodo);
         if ($metodo === 'GET') {
             $fn = $this->rutasGET[$urlActual] ?? null;
+            
         }
 
         //La URL existe y hay una funcion asociada
         if ($fn) {
+            /*
+            Usamos call_user_fun porque no sabemos cual va a ser la funcion que se va a ejecutar ya que
+            depende mucho de que URL visite el usuario.
+            El $this guarda la instancia del Router que contiene todas las URL
+            */
             call_user_func($fn, $this);
         }else{
             echo "No existe la Pagna 404";
--- public/index.php
+++ public/index.php
@@ -3,8 +3,18 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\PropiedadController;
+
 
     $router = new Router();
+
+    // debugear(PropiedadController::class); Devuelve en namespace de donde se encuentra esta funcion.
+    //Se identifica en que clase se encuentra el metodo
+
+    //Definimos las rutas:
+    $router->get('/admin', [PropiedadController::class, 'index']);
+    $router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
+    $router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 
     $router->comprobarRutas();
 
