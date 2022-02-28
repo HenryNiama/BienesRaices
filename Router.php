f12697a91eb6e31891a06510f07397b622892b20
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-02-28 12:28:11 -0500

    Creando el método para mostrar las Vistas

--- Router.php
+++ Router.php
@@ -41,6 +41,12 @@ class Router{
     }
 
 
+    //Muestra una vista
+    public function render($view)
+    {
+        include __DIR__ . "/views/$view.php";
+    }
+
     
 }
 
--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace Controllers;
+use MVC\Router;
+
+//El Controlador, se comunica con el Modelo, el Modelo con la Base de Datos, por ultimo, el Controlador comunica
+//Los resultados que dio el Modelo, a la Vista.
 
 class PropiedadController{
 
-    public static function index()
+    public static function index(Router $router)
     {
-        echo "Index";
+        $router->render('propiedades/admin');
     }
 
     public static function crear()
