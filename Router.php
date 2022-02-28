f0d4e509d15301e11960e9cc0f58110c2dfe0012
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-02-27 22:33:14 -0500

    Registrando URL's y los métodos asociados a ellos

--- Router.php
+++ Router.php
@@ -4,6 +4,37 @@
 namespace MVC;
 
 class Router{
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+
+
+    public function get($url, $fn)
+    {
+        $this->rutasGET[$url] = $fn;
+    }
+
+
+    public function comprobarRutas()
+    {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        // debugear($metodo);
+        if ($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        //La URL existe y hay una funcion asociada
+        if ($fn) {
+            call_user_func($fn, $this);
+        }else{
+            echo "No existe la Pagna 404";
+        }
+    }
+
+
     
 }
 
--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 
-$router = new Router();
+    $router = new Router();
+
+    $router->comprobarRutas();
 
 ?>
