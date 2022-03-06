766c511e9419bcbce4c8d3b99f44d394d16c9051
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-03-06 13:43:44 -0500

    Creando el modelo de Autenticación
    - Se edita el Admin.php y ojo, que esta esta asociada a la tabla 'usuarios' de la Base de datos.
    - Recomendación: Siempre que se tenga una tabla en la BD, siempre debe existir un modelo asociado a la misma.

--- models/Admin.php
+++ models/Admin.php
@@ -2,7 +2,25 @@
 
 namespace Model;
 
+
 class Admin extends ActiveRecord{
-    
+
+    //Base de datos
+    //Ojo que cada tabla de la Base de datos, tiene un modelo asociado.
+    //Y este Modelo Admin, tiene la tabla asociada de Usuarios.
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'email', 'password'];
+
+    public $id;
+    public $email;
+    public $password;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null; 
+        $this->email = $args['email'] ?? ''; 
+        $this->password = $args['password'] ?? ''; 
+    }
+
 }
 ?>
